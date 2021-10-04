fab881b5eb8688473bb3972cc13c8ddc4687ed6c
Selfie SD <83945969+Selfie-bd@users.noreply.github.com>
2021-10-04 08:58:35 +0530

    Update strings.py

--- InlineBot/strings.py
+++ InlineBot/strings.py
@@ -13,9 +13,9 @@ from InlineBot import (
 if CUSTOM_START_MESSAGE:
     START_MESSAGE = CUSTOM_START_MESSAGE
 else:
-    START_MESSAGE = """<b>Hello {mention},
+    START_MESSAGE = """<b>Hello Buddy {mention}!,
 
-I am an Inline Saver Bot, you can save inline filters and It can be use in any of your chats easily, Click help for more details</b> 
+I am an Inline Saver Bot, you can save inline filters and It can be use in any of your chats easily, Click /help for more details</b> 
 """
 
 HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
@@ -50,9 +50,9 @@ HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
 ABOUT_MESSAGE = f"""<b><u>ABOUT ME</u></b>
 
 <b>○ Maintained by : <a href='tg://user?id={OWNER_ID}'>This Person</a>
-○ Channel : <a href='https://t.me/CodeXBotz'>Code 𝕏 Botz</a>
-○ Support : <a href='https://t.me/CodeXBotzSupport'>Code 𝕏 Botz Support</a>
-○ Source Code : <a href='https://github.com/CodeXBotz/Inline-Filter-Bot'>Click here</a>
+○ Channel : <a href='https://t.me/umlinks'>Umlinks</a>
+○ Support : <a href='https://t.me/groupdc'>Group dc Bots</a>
+○ Source Code : <a href='https://github.com/selfie-bd/Inline-Filter-Bot'>Click here</a>
 ○ Language : <a href='https://www.python.org/'>Python 3</a>
 ○ Library : <a href='https://github.com/pyrogram/pyrogram'>Pyrogram Asyncio {__version__}</a></b>
 """
